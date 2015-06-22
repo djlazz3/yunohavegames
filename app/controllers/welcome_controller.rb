ec621bf28c134baf15b23c70f89d606b667f1559
Bryan <Bpadron2000@gmail.com>
2015-06-22 11:41:48 -0400

    removed initial admin setup

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,5 @@
 class WelcomeController < ApplicationController
   def index
-    if (current_user.try(:email) == "bpadron2000@gmail.com")
-      current_user.update_attribute :admin, true
-    end
+    
   end
 end
